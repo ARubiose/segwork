0d16e2428bf9a3e1d91f469d7dcca6b20ac09fcd
Álvaro Rubio Segovia <alvaro.rubio.segovia@gmail.com>
2022-06-19 18:19:33 +0200

    Clean repository

--- tests/registry_test.py
+++ tests/registry_test.py
@@ -9,11 +9,9 @@ class Testregistry(unittest.TestCase):
         self.registry = ConfigurableRegistry('model', )
         self.registry['test_entry'] = dict( model=str)
 
-    def test_sum(self):
-        self.assertEqual(type(self.registry[test_entry])), 6, "Should be 6")
+    def test_get_item(self):
+        self.assertEqual(self.registry['test_entry'], str, "Should be 6")
 
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
 
 if __name__ == '__main__':
     unittest.main()
