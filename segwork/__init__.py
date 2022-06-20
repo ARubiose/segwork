f63f6abd232af9c3aa0427dffa6428c93f4e0d8a
Álvaro Rubio Segovia <alvaro.rubio.segovia@gmail.com>
2022-06-20 13:11:00 +0200

    Changes

--- segwork/__init__.py
+++ segwork/__init__.py
@@ -1,9 +1,6 @@
 """SegWork is a set of computer vision tools for semantic segmentation
-
-
-
 """
-from .registry import ConfigurableRegistry
+from .registry import *
 from .data.balance import *
 from .data.transform import *
-from .data.dataset import SegmentationDataset
+from .data.dataset import *
